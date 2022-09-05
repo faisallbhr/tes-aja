bfcc136f589400c5c7bff3923b132a2138074c12
faisallbhr <fbhr1112@gmail.com>
2022-09-05 23:46:29 +0700

    edit gabut

--- grafkom.py
+++ grafkom.py
@@ -1,1 +1,10 @@
 print("cicak")
+
+x = 0
+while x <= 10 :
+    if x % 2 == 0:
+        print(x, "merupakan bilangan genap")
+    else:
+        print(x, "merupakan bilangan ganjil")
+    x += 1
+    
